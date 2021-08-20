344e1a2c118ed5c86383d7ee170ea753a85f1218
Luis Ciber <correaleyval@gmail.com>
2021-08-20 17:54:32 -0400

    Signing Config

--- lib/app/theme/dark.dart
+++ lib/app/theme/dark.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
 import 'package:todo/app/theme/app_bar_theme.dart';
 
-final darkTheme = ThemeData.dark().copyWith(
-  appBarTheme: appBarTheme
-);
+final darkTheme = ThemeData.dark().copyWith(appBarTheme: appBarTheme);
