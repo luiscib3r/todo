42c7aabf4337b30507987598399cd4fda10fd785
Luis Ciber <correaleyval@gmail.com>
2021-09-13 10:17:57 -0400

    Analytics

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:todo/app/app_router.dart';
@@ -22,6 +23,7 @@ class App extends StatelessWidget {
   final String env;
 
   Future<void> _init() async {
+    await Firebase.initializeApp();
     await setupDependencies(env);
   }
 
